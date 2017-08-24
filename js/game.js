48430db21dc3b82c7241571ce19e6544840ff5c3
Antonio Benincasa <benincasaantonio98@gmail.com>
2017-08-24 18:05:25 +0200

    The Ball was added: - Now it bounces around the game world - It collide with the platform and the block

--- js/game.js
+++ js/game.js
@@ -13,6 +13,7 @@ var block = {
 
 var blocks;
 var platform;
+var ball;
 var game = new Phaser.Game(gameScreen.width, gameScreen.height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var cursor;
 
@@ -22,29 +23,40 @@ function preload() {
   game.load.image("redBlock","assets/redBlock.png");
   game.load.image("greenBlock","assets/greenBlock.png");
   game.load.spritesheet("mediumPlatform","assets/mediumPlatform.png");
+  game.load.spritesheet("redBall","assets/redBall.png");
 }
 
 function create() {
-  cursor = game.input.keyboard.createCursorKeys();
   game.physics.startSystem(Phaser.Physics.ARCADE);
+  cursor = game.input.keyboard.createCursorKeys();
+
   blocks = game.add.group();
   createBlocks("green",24,30);
-  //createBlockss("red",15,98);
-  //createBlocks("blue",15,130);
-  //createBlocks("green",15,162);
-  platform = game.add.sprite(20,500,"mediumPlatform");
-  game.physics.arcade.enable([platform]);
 
+  platform = game.add.sprite(20,500,"mediumPlatform");
+
+  ball = game.add.sprite(20,500,"redBall");
+
+  game.physics.arcade.enable([blocks,ball,platform]);
+
+  platform.body.collideWorldBounds = true;
   platform.body.immovable = true;
+  blocks.enableBody = true;
+
+  ball.body.velocity.setTo(200, 200);
+  ball.body.collideWorldBounds = true;
+  ball.body.bounce.setTo(1, 1);
+
 }
 
 function update() {
-
+    game.physics.arcade.collide(platform,ball);
+    game.physics.arcade.collide(ball,blocks);
   platform.body.velocity.x = 0;
   if(cursor.left.isDown){
-    platform.body.velocity.x = -150;
+    platform.body.velocity.x = -250;
   }else if(cursor.right.isDown){
-    platform.body.velocity.x = 150;
+    platform.body.velocity.x = 250;
   }
 }
 
